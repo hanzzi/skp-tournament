4efeca03f60f800f1a0a7320ab629b5728aabcd8
Andreas Nielsen <thesilentwindow@live.dk>
2017-09-06 15:27:56 +0200

    Added codebehind.

---
+++ SkpEsport/SkpEsport/IndexPage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SkpEsport
+{
+    public partial class IndexPage : System.Web.UI.Page
+    {
+        
+        private string username, password;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Request.Form["txtFormValue"]))
+            {
+                username = Request.Form["inputUsername"];
+                password = Request.Form["inputPassword"];
+            }
+
+        }
+    }
+}
--- SkpEsport/SkpEsport/index.aspx
+++ SkpEsport/SkpEsport/index.aspx
@@ -55,6 +55,7 @@
                             <h2 class="form-signin-heading">Please sign in</h2>
                             <label for="inputEmail" class="sr-only">Email address</label>
                             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                            <asp:TextBox runat="server"></asp:TextBox>
                             <br>
                             <label for="inputPassword" class="sr-only">Password</label>
                             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
